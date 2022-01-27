225890685a58c88650d335dad21f1657b83eac2f
Hari haran <91955185+Hariharagr@users.noreply.github.com>
2022-01-27 17:47:31 +0530

    Update char1.py

--- char1.py
+++ char1.py
@@ -10,4 +10,25 @@ for x in s:
 print(output)
 
 
+s = 'ad12aswq55'
+output = ''
+num = ''
+i = 0
+while i<len(s):
+         if s[i].isalpha():
+             ch = s[i]
+         else:
+            num = num + s[i]
+            if i == len(s)-1:
+                output = output + ch*int(num)
+            if i+1 < len(s) and s[i+1].isalpha():
+                output = output + ch*int(num)
+                num = ''
+         i=i+1
+print(output)
+
+
+
+
+
 
